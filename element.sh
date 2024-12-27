429f02b8e2285eb23a44ac4af738ef31479896cf
Roquel7 <Roquel7@github.com>
2024-12-26 23:45:45 -0700

    feat: created a .gitignore file

---
+++ element.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  # Join the proerties and the elements tables
+
+  # you can search the atomic number, symbol, or name. 
+
+  # it will output the type, atomic mass, melting and boiling point.
+fi
