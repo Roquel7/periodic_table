ee4d8886a3d6bdc7ada280166e950cee60cecc93
Roquel7 <Roquel7@github.com>
2024-12-26 23:51:31 -0700

    creating repo

--- element.sh
+++ element.sh
@@ -4,7 +4,7 @@ then
   echo "Please provide an element as an argument."
 else
   # Join the proerties and the elements tables
-
+  echo "this works!"
   # you can search the atomic number, symbol, or name. 
 
   # it will output the type, atomic mass, melting and boiling point.
